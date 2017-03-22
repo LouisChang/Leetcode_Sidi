6e45b2bba2a41a980d67012d1dc89dd41414a4f9
LouisChang <changsidi@gmail.com>
2017-03-22 12:40:04 -0400

    Minor

--- 183_Customers_who_never_order.sql
+++ 183_Customers_who_never_order.sql
@@ -4,4 +4,4 @@ From Customers
 LEFT JOIN Orders
 On Customers.Id = Orders.CustomerId
 WHERE Orders.CustomerId is null
-# need left join because if the customer never orders, he will not be in the order list
+# Need left join because if the customer never orders, he will not be in the order list
