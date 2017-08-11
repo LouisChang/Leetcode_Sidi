313e77c0ab6266019740a7398781a11c374baa55
LouisChang <changsidi@gmail.com>
2017-08-11 15:40:44 -0400

    Valid anagram

--- 242_valid_anagram.py
+++ 242_valid_anagram.py
@@ -1,4 +1,6 @@
+"""
 class Solution(object):
+
     def isAnagram(self, s, t):
         """
         :type s: str
@@ -6,3 +8,22 @@ class Solution(object):
         :rtype: bool
         """
         return sorted(s) == sorted(t)
+ """
+
+from collections import Counter
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        a = Counter(s)
+        b = Counter(t)
+        c = a - b
+        d = b - a
+        return len(list(c)) == 0 and len(list(d)) == 0
+    
+    """
+    return sorted(s) == sorted(t)
+    """
