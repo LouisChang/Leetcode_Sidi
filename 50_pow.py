98ace42a4e440d9a4f30ab4c6a19488924011477
LouisChang <changsidi@gmail.com>
2017-08-11 15:48:10 -0400

    pow(x,n)

---
+++ 50_pow.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n < 0:
+            n = -n
+            x = 1/x
+        
+        if n % 2 == 0:
+            return self.myPow(x*x,n/2)
+        else:
+            return self.myPow(x*x,n/2)*x
